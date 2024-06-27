9018c6c22d45d524ef9abf5b49f0ccd07d0e5c1f
DESKTOP-3C3QM1O\LG CNS <tungnt@lgcns.com>
2024-06-27 15:59:03 +0700

    abc

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'dart:async';
 import 'dart:convert';
 
-import 'package:desktop_drop/desktop_drop.dart';
 import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:drag_and_drop_windows/drag_and_drop_windows.dart';
-import 'package:dragdropwindows/dragdropwindows.dart';
+// import 'package:dragdropwindows/dragdropwindows.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -15,81 +13,75 @@ void main(List<String> args) {
   }
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+    dropEventStream.listen((paths) {
+      print('MainApp dropEventStream: $paths');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: DropTarget(
-          onDragDone: (detail) {
-            print('onDragDone: $detail');
-          },
-          onDragEntered: (detail) {
-            print('onDragEntered: $detail');
-          },
-          onDragExited: (detail) {
-            print('onDragExited: $detail');
-          },
-          child: Center(
-            child: TextButton(
-                onPressed: () async {
-                  final window =
-                      await DesktopMultiWindow.createWindow(jsonEncode({
-                    'args1': 'multi_window',
-                    'args2': 100,
-                    'args3': true,
-                    'bussiness': 'bussiness_test',
-                  }));
-                  window
-                    ..setFrame(const Offset(0, 0) & const Size(1280, 720))
-                    ..center()
-                    ..setTitle('Another window')
-                    ..show();
-                },
-                child: const Text('Show Sub window')),
-          ),
+        body: Center(
+          child: TextButton(
+              onPressed: () async {
+                final window =
+                    await DesktopMultiWindow.createWindow(jsonEncode({
+                  'args1': 'multi_window',
+                  'args2': 100,
+                  'args3': true,
+                  'bussiness': 'bussiness_test',
+                }));
+                window
+                  ..setFrame(const Offset(0, 0) & const Size(400, 400))
+                  ..center()
+                  ..setTitle('Another window')
+                  ..show();
+              },
+              child: const Text('Show Sub window')),
         ),
       ),
     );
   }
 }
 
-class SubApp extends StatelessWidget {
+class SubApp extends StatefulWidget {
   const SubApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    StreamSubscription subscription = dropEventStream.listen((paths) {
-      print('dropEventStream: $paths');
+  State<SubApp> createState() => _SubAppState();
+}
+
+class _SubAppState extends State<SubApp> {
+  @override
+  void initState() {
+    super.initState();
+    dropEventStream.listen((paths) {
+      print('SubApp dropEventStream: $paths');
     });
+  }
 
-    try {
-      Dragdropwindows.start((DropType type, List<String> results) {
-        print("Dragdropwindows type=$type results=$results");
-      });
-    } catch (e) {}
-
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: DropTarget(
-            onDragDone: (detail) {
-              print('onDragDone: $detail');
-            },
-            onDragEntered: (detail) {
-              print('onDragEntered: $detail');
-            },
-            onDragExited: (detail) {
-              print('onDragExited: $detail');
-            },
-            child: TextButton(
-                onPressed: () async {
-                  print('Sub app');
-                },
-                child: const Text('Sub window')),
-          ),
+          child: TextButton(
+              onPressed: () async {
+                print('Sub app');
+              },
+              child: const Text('Sub window')),
         ),
       ),
     );
